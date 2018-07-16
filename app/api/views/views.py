4d0b18578c2acdea2ca43009cbcf28e540bd9684
RachaelNantale <rachaelminani42@gmail.com>
2018-07-16 19:09:43 +0300

    [#159078807]Add more methods

--- app/api/views/views.py
+++ app/api/views/views.py
@@ -34,7 +34,46 @@ class RequestList(Resource):
         }), 201)
 
 
+class Request(Resource):
+
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('requests', type=str, required=True,
+                                   help='No request provided',
+                                   location='json')
+        self.reqparse.add_argument('type', type=str, required=True,
+                                   help='Type provided',
+                                   location='json')
+        super(Request, self).__init__()
+
+    def get(self, id):
+        for request in REQUESTS:
+            if request.get_id() == id:
+
+                return make_response(jsonify(
+                    request.to_json()
+                ), 200)
+        return make_response(jsonify({"message": "Ride not found."}), 404)
+
+    def put(self, id):
+        task = [request for request in REQUESTS if request.get_id() == id]
+        if len(task) == 0:
+            abort(404)
+        request = task[0]
+        args = self.reqparse.parse_args()
+        for k, v in args.items():
+            if k is not 'id':
+                setattr(request, k, v)
+               
+        return make_response(jsonify(request.to_json()), 201)
+
+    def delete(self, id):
+        task = [request for request in REQUESTS if request.get_id() == id]
+        if len(task) == 0:
+            abort(404)
+        REQUESTS.remove(task[0])
+        return {'result': True}
 
 
 api.add_resource(RequestList, '/api/v1/requests')
-
+api.add_resource(Request, '/api/v1/requests/<string:id>')
